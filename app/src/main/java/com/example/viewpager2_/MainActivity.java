5e0228c9e4254968d9af071847ec440e0109c01f
robin.singla <robinformal@gmail.com>
2021-04-23 21:30:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewpager2_/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.viewpager2_;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    ArrayList<ViewPagerItem> viewPagerItemArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewpager);
+        int[] images = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e};
+        String[] heading = {"Baked","Grilled","Dessert","Italian","Shakes"};
+        String[] desc = {getString(R.string.a_desc),
+                getString(R.string.b_desc),
+                getString(R.string.c_desc),
+                getString(R.string.d_desc)
+        ,getString(R.string.e_desc)};
+
+        viewPagerItemArrayList = new ArrayList<>();
+
+        for (int i =0; i< images.length ; i++){
+
+            ViewPagerItem viewPagerItem = new ViewPagerItem(images[i],heading[i],desc[i]);
+            viewPagerItemArrayList.add(viewPagerItem);
+
+        }
+
+        VPAdapter vpAdapter = new VPAdapter(viewPagerItemArrayList);
+
+        viewPager2.setAdapter(vpAdapter);
+
+        viewPager2.setClipToPadding(false);
+
+        viewPager2.setClipChildren(false);
+
+        viewPager2.setOffscreenPageLimit(2);
+
+        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+    }
+}
---
+++ app/src/main/java/com/example/viewpager2_/ViewPagerItem.java
@@ -0,0 +1,13 @@
+package com.example.viewpager2_;
+
+public class ViewPagerItem {
+
+    int imageID;
+    String heading, description;
+
+    public ViewPagerItem(int imageID, String heading, String description) {
+        this.imageID = imageID;
+        this.heading = heading;
+        this.description = description;
+    }
+}
